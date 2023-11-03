8876c77db42c1ec86561bfa1326ebd222e74aeb1
Sergio Rondon <sergio19980816@gmail.com>
2023-11-02 23:05:41 -0500

    adding view for categories

--- FinanceApp/Models/Category.cs
+++ FinanceApp/Models/Category.cs
@@ -8,6 +8,7 @@ namespace FinanceApp.Models
         [Required(ErrorMessage = "El campo {0} es requerido")]
         [StringLength(maximumLength:50, ErrorMessage = "No puede ser mayor a {1} caracteres")]
         public string Name { get; set; }
+        [Display(Name = "Tipo Operacion")]
         public OperationType OperationTypeId { get; set; }
         public int UserId { get; set; }
     }
